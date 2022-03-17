b094e75382910488474a17de92c9d148df9c35fa
Evgenii <v2454802@gmail.com>
2022-03-17 15:34:06 +0300

    register-newDB

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const FileStore = require('session-file-store')(session);
 
+const hbs = require('hbs');
+
+hbs.registerPartials(path.join(process.env.PWD, 'views', 'partials'));
+
+const routerHome = require('./routers/home');
+const routerRegistration = require('./routers/registration');
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
@@ -27,8 +34,10 @@ const sessionConfig = {
 };
 app.use(session(sessionConfig));
 
-app.use((req, res, next) => {
-  const error = createError(404, 'Запрашиваемой страницы не существует');
-  next(error);
+app.use('/', routerHome);
+app.use('/registration', routerRegistration);
+
+app.use((req, res) => {
+  res.status(404).json('Запрашиваемой страницы не существует');
 });
 app.listen(PORT, () => console.log('started on port:', PORT));
