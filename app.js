27824286c79509919b17d5222193d8562aad284b
Mark <smirnovsd19@mail.ru>
2022-03-17 14:47:05 +0300

    1

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const path = require('path');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const FileStore = require('session-file-store')(session);
+const hbs = require('hbs');
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+// app.set('scripts', path.join(__dirname, 'scripts'));
+hbs.registerPartials(path.join(process.env.PWD, 'views', 'partials'));
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -26,9 +29,15 @@ const sessionConfig = {
   saveUnitialized: false,
 };
 app.use(session(sessionConfig));
-
-app.use((req, res, next) => {
-  const error = createError(404, 'Запрашиваемой страницы не существует');
-  next(error);
+app.get('/', (req, res) => {
+  res.render('home');
 });
+app.get('/registration', (req, res) => {
+  res.render('registration');
+});
+
+// app.use((req, res, next) => {
+//   const error = createError(404, 'Запрашиваемой страницы не существует');
+//   next(error);
+// });
 app.listen(PORT, () => console.log('started on port:', PORT));
--- public/scripts/register.js
+++ public/scripts/register.js
@@ -1,14 +1,16 @@
-const {formReg} = document.forms
-formReg.addEventListener('submit', async (e) => {
+const { regForm } = document.forms
+regForm.addEventListener('submit', async (e) => {
   e.preventDefault()
+  // console.log(regForm.roleId.value);
   const response = await fetch('/registration', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({login: login.value, email: email.value, pass: pass.value, role: roleId.value})
+    body: JSON.stringify({ login: login.value, email: email.value, pass: pass.value, role: roleId.value })
   })
-  if(response.ok) {
-    window.location = 'hhtp://localhost:3000/signup'
-  }
+
+  // if(response.ok) {
+  //   window.location = 'hhtp://localhost:3000/signup'
+  // }
 })
