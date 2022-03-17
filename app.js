69d934af18dd2cf4a9a3eacfefb5f3a2b3be82af
Anton <anton_st_01_02@mail.ru>
2022-03-17 16:44:38 +0500

    add2 hbs

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const path = require('path');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const FileStore = require('session-file-store')(session);
-
+const hbs = require('hbs');
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(process.env.PWD, 'views', 'partials'));
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -27,8 +28,16 @@ const sessionConfig = {
 };
 app.use(session(sessionConfig));
 
-app.use((req, res, next) => {
-  const error = createError(404, 'Запрашиваемой страницы не существует');
-  next(error);
+app.get('/registration', (req, res) => {
+  res.render('registration');
+});
+app.get('/home', (req, res) => {
+  res.render('home');
+});
+app.get('/signin', (req, res) => {
+  res.render('signin');
+});
+app.use((req, res) => {
+  res.status(404).send('not found');
 });
 app.listen(PORT, () => console.log('started on port:', PORT));
